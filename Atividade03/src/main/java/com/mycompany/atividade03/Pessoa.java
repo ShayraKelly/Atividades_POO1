ebe79e8e8c1d7460710c093201c97344cd88b75f
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-03-22 17:58:56 -0300

    Update Pessoa.java

--- Atividade03/src/main/java/com/mycompany/atividade03/Pessoa.java
+++ Atividade03/src/main/java/com/mycompany/atividade03/Pessoa.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shayr
+ */
+
 
 public class Pessoa {
     public static void main(String[] args) {
