cf770a85d1d3389790513225a98fda68906f6a4b
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-03-24 19:44:43 -0300

    Update

--- Atividade03/src/main/java/com/mycompany/atividade03/Pessoa.java
+++ Atividade03/src/main/java/com/mycompany/atividade03/Pessoa.java
@@ -8,9 +8,30 @@
  * @author shayr
  */
 
-
 public class Pessoa {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private String nome;
+    private String cpf;
+    
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
     }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
 }
