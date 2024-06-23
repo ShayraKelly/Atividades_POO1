5b28c2b8791e2451809177bef4bb2ba48e7af1f4
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-06-23 20:35:53 -0300

    Update

--- Atividade_05/Usuario_especial.java
+++ Atividade_05/Usuario_especial.java
@@ -1,12 +1,14 @@
 public class Usuario_especial extends Usuario{
 //atributos
-    public String resumo;
 
 //metodos 
-    public Usuario_especial(String nome, int telefone, String email, String cpf) {
-        super(titulo, dt_publicacao, publicacoes_ref, autor);
-        this.edicao = edicao;
-        this.editora = editora;
-        this.isbn = isbn;
+    public Usuario_especial(String nome, int telefone, String email, String cpf, int emprestimo) {
+        super(nome, telefone, email, cpf, emprestimo);
+    }
+
+    public boolean verifica_emprestimo(int qtd_livro){
+        int emprestimo = getEmprestimo();
+        setEmprestimo(emprestimo++);
+        return true;
     }
 }
