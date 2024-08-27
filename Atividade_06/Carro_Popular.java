516a673493f7f6fa8719c308e3b6cce3441a7743
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-08-27 19:58:51 -0300

    Update Carro_Popular.java

--- Atividade_06/Carro_Popular.java
+++ Atividade_06/Carro_Popular.java
@@ -1,24 +1,17 @@
-public class Carro_Popular extends Carro {
-    private String ar_condicionado;
+public class Carro_Popular extends Carro{
+    private boolean ar_condicionado;
 
-    public Carro_Popular(String marca, String modelo, String placa, String valor_locacao, int ano,
-            String ar_condicionado) {
+    public Carro_Popular(String marca, String modelo, String placa, double valor_locacao, int ano,
+            boolean ar_condicionado) {
         super(marca, modelo, placa, valor_locacao, ano);
         this.ar_condicionado = ar_condicionado;
     }
 
-    public String getAr_condicionado() {
+    public boolean isAr_condicionado() {
         return ar_condicionado;
     }
 
-    public void setAr_condicionado(String ar_condicionado) {
-        if (ar_condicionado = "S") {
-            ar_condicionado = true;
-        }if (ar_condicionado = "N") {
-            ar_condicionado = false;
-        } else {
-            System.err.println("Erro: " + e.getMessage());   
-        }
-    } 
-    
+    public void setAr_condicionado(boolean ar_condicionado) {
+        this.ar_condicionado = ar_condicionado;
+    }   
 }
