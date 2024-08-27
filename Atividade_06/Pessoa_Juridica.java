a334d8a0e15b6464ac4f24215149711092b78f8e
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-08-27 19:56:57 -0300

    Update Pessoa_Juridica.java

--- Atividade_06/Pessoa_Juridica.java
+++ Atividade_06/Pessoa_Juridica.java
@@ -5,12 +5,20 @@ public class Pessoa_Juridica implements Cliente{
     private String cnpj;
     private String nome_representante;
 
+    public Pessoa_Juridica(String nome, String telefone, String endereco, String cnpj, String nome_representante) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.cnpj = cnpj;
+        this.nome_representante = nome_representante;
+    }
+
     @Override
     public String getNome(){
         return nome;
     }
 
-    @override
+    @Override
     public String getTelefone(){
         return telefone;
     }
@@ -18,16 +26,6 @@ public class Pessoa_Juridica implements Cliente{
     @Override
     public String getEndereco(){
         return endereco;
-    }
-
-    public Pessoa_Juridica(String nome, String cnpj, String nome_representante) {
-        this.nome = nome;
-        this.cnpj = cnpj;
-        this.nome_representante = nome_representante;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getCnpj() {
@@ -45,5 +43,5 @@ public class Pessoa_Juridica implements Cliente{
     public void setNome_representante(String nome_representante) {
         this.nome_representante = nome_representante;
     }
-    
+
 }
